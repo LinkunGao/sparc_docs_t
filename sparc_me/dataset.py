35c076fa0527e3d15189c208c24d3ad404d320dd
skycoco <skycoco1994@gmail.com>
2023-09-20 16:31:54 +1200

    sa

--- sparc_me/dataset.py
+++ sparc_me/dataset.py
@@ -630,52 +630,52 @@ class Dataset(object):
 
         return metadata
 
-    # def _generate_file_from_template(self, save_path, metadata_file, data=pd.DataFrame(), keep_style=False):
-    #     """Generate file from a template and populate with data if givn
-    #     TODO: will delete later
-    #
-    #     :param save_path: destination to save the generated file
-    #     :type save_path: string
-    #     :param metadata_file: SDS metadata_file (Ex: samples, subjects)
-    #     :type metadata_file: string
-    #     :param data: pandas dataframe containing data, defaults to pd.DataFrame()
-    #     :type data: pd.DataFrame, optional
-    #     """
-    #
-    #     if keep_style:
-    #         self._template_dir = self._get_template_dir(version=self._version)
-    #         sf = StyleFrame.read_excel_as_template(os.path.join(self._template_dir, f'{metadata_file}.xlsx'), data)
-    #         writer = StyleFrame.ExcelWriter(save_path)
-    #         sf.to_excel(writer)
-    #         writer.save()
-    #     else:
-    #         data.to_excel(save_path, index=False)
-    #
-    # """***************************New Add subjects ***************************"""
-    #
-    # def add_subjects(self, subjects):
-    #
-    #     """
-    #     Add Subejct list to dataset.
-    #     This function will add subjects and samples to metadata,
-    #     And will move the sample files from origin source path to dataset
-    #     primary subject sample folder.
-    #     It will automatically update manifest and dataset_description metadata files.
-    #
-    #     :param subjects: Subject dataset
-    #     :type subjects: list
-    #     """
-    #
-    #     self.save()
-    #     if not isinstance(subjects, list):
-    #         msg = "Please provide a list of subjects"
-    #         raise ValueError(msg)
-    #     for subject in subjects:
-    #         self._subjects[subject.subject_id] = subject
-    #         subject.move()
-    #
-    #     self._update_sub_sam_nums_in_dataset_description(self._dataset_path / 'primary')
-    #
+    def _generate_file_from_template(self, save_path, metadata_file, data=pd.DataFrame(), keep_style=False):
+        """Generate file from a template and populate with data if givn
+        TODO: will delete later
+
+        :param save_path: destination to save the generated file
+        :type save_path: string
+        :param metadata_file: SDS metadata_file (Ex: samples, subjects)
+        :type metadata_file: string
+        :param data: pandas dataframe containing data, defaults to pd.DataFrame()
+        :type data: pd.DataFrame, optional
+        """
+
+        if keep_style:
+            self._template_dir = self._get_template_dir(version=self._version)
+            sf = StyleFrame.read_excel_as_template(os.path.join(self._template_dir, f'{metadata_file}.xlsx'), data)
+            writer = StyleFrame.ExcelWriter(save_path)
+            sf.to_excel(writer)
+            writer.save()
+        else:
+            data.to_excel(save_path, index=False)
+
+    """***************************New Add subjects ***************************"""
+
+    def add_subjects(self, subjects):
+
+        """
+        Add Subejct list to dataset.
+        This function will add subjects and samples to metadata,
+        And will move the sample files from origin source path to dataset
+        primary subject sample folder.
+        It will automatically update manifest and dataset_description metadata files.
+
+        :param subjects: Subject dataset
+        :type subjects: list
+        """
+
+        self.save()
+        if not isinstance(subjects, list):
+            msg = "Please provide a list of subjects"
+            raise ValueError(msg)
+        for subject in subjects:
+            self._subjects[subject.subject_id] = subject
+            subject.move()
+
+        self._update_sub_sam_nums_in_dataset_description(self._dataset_path / 'primary')
+
     # def get_subject(self, subject_sds_id) -> Subject:
     #     """
     #     Get a subject by subject sds id
