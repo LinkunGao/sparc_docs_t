f3e1e5859504b53051a6c4fc9528ae4dd2b6741a
skycoco <skycoco1994@gmail.com>
2023-09-20 12:12:22 +1200

    da

--- mypro/metadata.py
+++ mypro/metadata.py
@@ -1,3 +1,22 @@
+"""Bb模块的文档注释"""
+
+
+class Bb(object):
+    """
+    Bb类的注释
+    """
+
+    @staticmethod
+    def bb_api(x, y):
+        """
+        求商
+        >>> Bb.bb_api(2, 4)
+        0.5
+        >>> Bb.bb_api(4, 2)
+        2.0
+        """
+        return x / y
+
 import pandas as pd
 from pathlib import Path
 from sparc_me.core.utils import remove_spaces_and_lower
